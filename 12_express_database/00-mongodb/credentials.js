24da3df7c9cfb29d386aa3a72fee27cc67c1bfee
Alexey Palhanov <palhanov@gmail.com>
2021-05-11 14:26:05 +0300

    third edition

---
+++ 12_express_database/00-mongodb/credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+	cookieSecret: 'coming strength nothing half',
+	mongo: {
+		development: {
+			connectionString: "mongodb+srv://palhanov:724hj6@fullstack.dgb2v.mongodb.net/app?retryWrites=true&w=majority"
+		}
+	}
+}
